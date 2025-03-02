407077fc32cd8cdccf713103fdef961c5c884e52
Nikita V <swebs.sh@gmail.com>
2025-03-03 02:33:34 +0500

    Добавил кнопку паузы для слайдера отзывов

--- resources/views/pages/home/components/feedback.blade.php
+++ resources/views/pages/home/components/feedback.blade.php
@@ -2,16 +2,26 @@
     reviews: {{ json_encode($feedbacks) }},
     currentIndex: 0,
     timer: null,
+    isPlaying: true,
     init() {
         this.startSlideshow();
     },
     startSlideshow() {
+        this.isPlaying = true;
         this.timer = setInterval(() => {
             this.nextSlide();
         }, 5000); // Смена слайда каждые 5 секунд
     },
     stopSlideshow() {
+        this.isPlaying = false;
         clearInterval(this.timer);
+    },
+    toggleSlideshow() {
+        if (this.isPlaying) {
+            this.stopSlideshow();
+        } else {
+            this.startSlideshow();
+        }
     },
     restartSlideshow() {
         this.stopSlideshow();
@@ -38,8 +48,8 @@
                          x-transition:leave="transition ease-in-out duration-500" x-transition:leave-start="opacity-100"
                          x-transition:leave-end="opacity-0">
                 </div>
-                <div class="bg-white -translate-y-[50px] xl:-translate-y-[0px] xl:-translate-x-[55px] p-[40px] xl:w-[850px] h-auto xl:h-[410px] rounded-[30px] relative"
-                     @mouseenter="stopSlideshow()" @mouseleave="startSlideshow()">
+                <div
+                    class="bg-white -translate-y-[50px] xl:-translate-y-[0px] xl:-translate-x-[55px] p-[40px] xl:w-[850px] h-auto xl:h-[410px] rounded-[30px] relative">
                     <img src="/assets/images/cliparts/wave-03.png" alt=""
                          class="absolute right-[0px] top-[0px] h-full z-[1]">
                     <div class="flex items-center relative z-[5]">
@@ -54,8 +64,9 @@
                                  x-text="reviews[currentIndex].about"></div>
                         </div>
                     </div>
-                    <div class="h-[114px] mt-[20px] xl:mt-[40px] relative z-[5] overflow-y-auto pr-[8px]">
-                        <p class="text-[14px] md:text-[24px] text-custom-heading" x-text="reviews[currentIndex].message"></p>
+                    <div class="h-auto xl:h-[114px] mt-[20px] xl:mt-[40px] relative z-[5] xl:overflow-y-auto pr-[8px]">
+                        <p class="text-[14px] md:text-[24px] text-custom-heading"
+                           x-text="reviews[currentIndex].message"></p>
                     </div>
                     <div class="mt-[20px] xl:mt-[40px] flex items-center justify-between relative z-[5]">
                         <div class="flex items-center">
@@ -65,12 +76,17 @@
                             </template>
                         </div>
                         <div class="flex items-center">
+                            <button @click="toggleSlideshow()"
+                                    class="border flex justify-center items-center border-[#E2E2E2] w-[35px] xl:w-[64px] h-[35px] xl:h-[64px] rounded-full text-[10px] xl:text-[14px] text-white hover:text-custom-main mr-[12px] bg-custom-main hover:bg-custom-halftone transition-colors duration-300">
+                                <i class="fas"
+                                   :class="{'fa-pause': !isPlaying, 'fa-play': isPlaying}"></i>
+                            </button>
                             <button @click="prevSlide()"
                                     class="border flex justify-center items-center border-[#E2E2E2] w-[35px] xl:w-[64px] h-[35px] xl:h-[64px] rounded-full text-[16px] xl:text-[30px] text-white hover:text-custom-main mr-[12px] bg-custom-main hover:bg-custom-halftone transition-colors duration-300">
                                 <i class="fas fa-angle-left translate-y-[1px]"></i>
                             </button>
                             <button @click="nextSlide()"
-                                    class="border flex justify-center items-center border-[#E2E2E2] w-[35px] xl:w-[64px] h-[35px] xl:h-[64px] rounded-full text-[16px] xl:text-[30px] text-white hover:text-custom-main mr-[12px] bg-custom-main hover:bg-custom-halftone transition-colors duration-300">
+                                    class="border flex justify-center items-center border-[#E2E2E2] w-[35px] xl:w-[64px] h-[35px] xl:h-[64px] rounded-full text-[16px] xl:text-[30px] text-white hover:text-custom-main bg-custom-main hover:bg-custom-halftone transition-colors duration-300">
                                 <i class="fas fa-angle-right translate-y-[1px]"></i>
                             </button>
                         </div>
