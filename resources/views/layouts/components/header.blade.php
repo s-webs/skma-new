3e70e3ff373184b911bd10c7cabf8cbbe81731d1
s-webs <swebs.sh@gmail.com>
2025-03-07 14:53:03 +0500

    Изменил вставку SVG

--- resources/views/layouts/components/header.blade.php
+++ resources/views/layouts/components/header.blade.php
@@ -35,8 +35,10 @@
         <div class="container px-4 2xl:px-[16px] mx-auto flex items-center">
             <div class="w-[104px] relative">
                 <a href="{{ route('home') }}"
-                   class="flex items-center justify-center bg-custom-main w-[74px] md:w-[104px] h-[70px] md:h-[100px] absolute z-10 top-[-29px] md:top-[-28px] lg:top-[-39px] left-1/2 -translate-x-1/2 lg:left-[0px] lg:translate-x-0 rounded-bl-2xl rounded-br-2xl shadow-md">
-                    <img src="/assets/images/logos/logo.svg" alt="logo" class="w-[36px] md:w-[48px]">
+                   class="flex items-center justify-center bg-custom-main w-[74px] md:w-[104px] h-[70px] md:h-[100px] absolute z-10 top-[-29px] md:top-[-28px] lg:top-[-39px] left-1/2 -translate-x-1/2 lg:left-[0px] lg:translate-x-0 rounded-bl-2xl rounded-br-2xl shadow-md pb-[10px] pt-[5px] lg:pb-[20px] lg:pt-[8px]">
+                    <object data="/assets/images/logos/logo.svg" type="image/svg+xml" class="w-full h-full">
+                        <img src="/assets/images/logos/logo.png" alt="logo">
+                    </object>
                 </a>
             </div>
             <div class="flex-1"></div>
