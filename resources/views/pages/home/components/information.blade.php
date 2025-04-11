32db80aa5901dee8f1a5c78e0543776bc30de8f1
Nikita V <swebs.sh@gmail.com>
2025-04-11 10:48:05 +0500

    Начал работу над страницей комплаенс службы

--- resources/views/pages/home/components/information.blade.php
+++ resources/views/pages/home/components/information.blade.php
@@ -1,4 +1,4 @@
-<div class="relative bg-[var(--color-main)] 2xl:h-[355px] overflow-hidden">
+<div class="relative bg-[var(--color-main)] 2xl:h-[355px]">
     @if($activeTheme->code === 'default')
         <img src="/assets/images/cliparts/wave-02.png" alt="" class="absolute w-full h-full object-cover z-[3]">
     @else
@@ -9,21 +9,21 @@
             <div class="py-[16px] md:py-[0px]">
                 <div class="text-[14px] md:text-[24px] 2xl:text-[36px] text-white font-bold">
                     <h3>
-                        {{ __('applicant.information_for_applicants') }}
+                        Комплаенс служба
                     </h3>
                 </div>
-                <div class="text-white text-[10px] md:text-[16px] mt-[10px] md:mt-[24px]">
-                    <p>
-                        {{ __('applicant.information_about_admission_documents') }}
-                    </p>
-                </div>
+{{--                <div class="text-white text-[10px] md:text-[16px] mt-[10px] md:mt-[24px]">--}}
+{{--                    <p>--}}
+{{--                        {{ __('applicant.information_about_admission_documents') }}--}}
+{{--                    </p>--}}
+{{--                </div>--}}
                 <div class="mt-[16px] md:mt-[32px] 2xl:mt-[64px]">
                     <a href="{{ route('applicant.index') }}"
                        class="px-[16px] md:px-[31px] py-[8px] md:py-[21px] bg-white hover:bg-[var(--color-main)] text-[12px] md:text-[18px] font-semibold text-[var(--color-main)] hover:text-white transition-all duration-300 rounded-full">{{ __('home.detail') }}</a>
                 </div>
             </div>
-            <div class="w-[248px] translate-y-[27px]">
-                <img src="/assets/images/cliparts/student_01.png" alt="">
+            <div class="w-[400px] h-[400px] border-[20px] -translate-y-[20px] border-[var(--color-extra)] rounded-[10px] overflow-hidden bg-[var(--color-extra)]">
+                <img src="/assets/images/komplaens_01.jpg" alt="" class="w-full h-full object-cover rounded-[10px]">
             </div>
         </div>
     </div>
