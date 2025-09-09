f3e50021458c6ca4ab2d91ca55072c37d8edf1b2
Nikita V <swebs.sh@gmail.com>
2025-09-09 19:51:21 +0500

    Работаю над api новостей для лайков

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'news_id',
+        'created_at',
+        'updated_at'
+    ];
+
     public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(User::class);
